638cfac26d5ecfe7a52968f647091da0be3fc36c
Nick Defrunct <nick.defrunct@gmail.com>
2024-09-16 20:20:07 +0300

    Added Axios
    Додав Axios для виконання запитів до OWM API.
    Може знадобитися виконання _install.cmd. Якщо не спрацює, виконайте "npm install axios --save".
    
    Додав тестовий запит з виведенням даних. Щоб побачити початковий екран змініть "tryGetWeather" на "false" (буде видалено пізніше, коли буде нормальна логіка)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,13 +4,14 @@
     <div class="col q-pt-lg q-px-md">
       <q-input 
       filled bottom-slots v-model="search" 
-      label="Пошук" 
+      label="Місто" 
       >
 
           <template v-slot:before>
             <q-icon name="my_location"/>
           </template>
 
+          <!-- Підказка дійсно потрібна? -->
           <template v-slot:hint>
             Підказка поля
           </template>
@@ -24,16 +25,18 @@
     <template v-if="weatherData">
 
       <div class="col text-white text-center">
-        <div class="text-h4 text-weight-light custom-font">
-          Відень 
-          <!-- {{ weatherData.name  }} -->
+        <div class="text-h2 text-weight-light custom-font">
+          <!-- Відень  -->
+          {{ weatherData.name  }}
         </div>
         <div class="text-h6 text-weight-light custom-font">
-          Дощ
-          <!-- {{ weatherData.weather[0].main }} -->
+          <!-- Дощ -->
+          {{ weatherData.weather[0].description.toUpperCase() }}
         </div>
         <div class="text-h1 text-weight-thin custom-font q-my-lg relative-position">
-          <span>13<!-- {{Math.round(weatherData.main.temp) }} -->
+          <span>
+            <!-- 13 -->
+            {{ Math.ceil(weatherData.main.temp) }}
           </span>
           <span class="text-h4 relative-position degree" >&deg;C</span>
         </div>
@@ -41,12 +44,13 @@
     
 
     <div class="col text-center">
-      <img :src="`https://openweathermap.org/img/wn/10n@2x.png`" >
-      <!--<img :src="`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" >  -->
+      <!-- <img :src="`https://openweathermap.org/img/wn/10n@2x.png`" > -->
+      <img :src="`https://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`" /> 
     </div>
 
     </template>
 
+    <!-- Змінюйте tryGetWeather для тестування -->
     <template v-else>
       <div class="col text-center text-white custom-font">
         <div class="col text-h2 text-white custom-font text-weight-thin">
@@ -75,10 +79,22 @@
 
 defineOptions({
   name: 'IndexPage',
-    data(){
+    data() {
       return{
         search:'',
-        weatherData: true
+        weatherData: null,
+        tryGetWeather: true
+      }
+    },
+
+    mounted() {
+      if (this.tryGetWeather) {
+        this.$axios(
+          `https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=dd76f1d7c2c54c9250d0de544316dcca&units=metric&lang=ua`
+        ).then(response => {
+          console.log("response: ", response)
+          this.weatherData = response.data
+        })
       }
     }
 });
