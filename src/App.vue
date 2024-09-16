83d4993244a621bf109e3940c027fe0a99a3215d
Maryna <marimay228@gmail.com>
2024-09-16 16:25:46 +0200

    add background

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <router-view />
+  <q-layout>
+    <q-page-container class="main-layout">
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
 defineOptions({
   name: 'App'
-});
+})
 </script>
+
+<style lang="scss">
+  .main-layout {
+    background: linear-gradient(to left, #fff, #076585);
+  }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page class="flex column">
+  <div class="col q-pt-lg q-px-md">
+    <q-input 
+    filled bottom-slots v-model="search" 
+    label="Search" 
     >
+
+        <template v-slot:before>
+          <q-icon name="my_location"/>
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+  </div>
   </q-page>
 </template>
 
 <script setup>
+
 defineOptions({
-  name: 'IndexPage'
+  name: 'IndexPage',
+    data(){
+      return{
+        search:''
+      }
+    }
 });
 </script>
+
