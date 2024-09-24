d54f32407ab54d943c4bac783b159e3d8f746e8d
Maryna <marimay228@gmail.com>
2024-09-24 18:55:58 +0200

    Add favorites icons
    Read the comments, I was a little lazy

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -16,7 +16,7 @@
     </div>
 
     <template v-if="weatherData">
-
+<!-- Тиць тиць on the button and go to favorites -->
       <div class="col text-white text-right custom-font text-sha">
           <q-btn
             size="lg"
@@ -32,7 +32,7 @@
         <div class="text-h2 text-weight-light custom-font text-sha d-flex justify-center items-center">
         <!-- Місто  -->
         <span>{{ weatherData.name }}</span>
-        <!-- при натиску зміити на   icon="favorite" -->
+        <!-- при натиску зміити на icon="favorite" -->
           <q-btn
           size="lg"
           round
@@ -60,7 +60,6 @@
         <!-- <img :src="`https://openweathermap.org/img/wn/10n@2x.png`" > -->
         <!-- <img :src="`https://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`" /> -->
 
-        <!-- TODO: Змінити стилі -->
         <div class="weather-icon">
           <img :src="getWeatherIconReplacer(weatherData.weather[0].icon)" />
         </div>
@@ -104,7 +103,75 @@
       </div>
     </template>
 
+<!-- Add normal if.   -->
+    <template v-if="favorites">
+      
+      <div class="col text-center text-white custom-font">
+        <div class="col text-h2 text-white custom-font text-weight-thin text-sha">
+          Збережене
+        </div>
+      </div>
+<!-- Add for. A made just stable schit -->
+      <div class="q-pa-md"> 
+        <div class="weather-container row q-my-md q-px-md q-py-sm q-gutter-md justify-between items-center rounded-borders">
+          <div class="col text-left">
+            <div class="text-h3 text-weight-light custom-font text-white">
+            Відень
+            </div>
+          </div>
 
+          <div class="col text-center">
+            <div class="text-h3 text-weight-light custom-font text-white">
+               Дощ
+            </div>
+          </div>
+
+          <div class="col text-center">
+            <div class="text-h1 text-white custom-font">
+              22&deg;C
+            </div>
+          </div>
+
+          <div class="weather-icon">
+            <!-- <img :src="getWeatherIconReplacer(weatherData.weather[0].icon)" /> -->
+            <img src="https://cdn-icons-png.flaticon.com/128/4814/4814268.png" />
+          </div>
+
+        
+        </div>
+      </div>
+      
+      <!-- Remove this crutch -->
+      <div class="q-pa-md"> 
+        <div class="weather-container row q-my-md q-px-md q-py-sm q-gutter-md justify-between items-center rounded-borders">
+          <div class="col text-left">
+            <div class="text-h3 text-weight-light custom-font text-white">
+            Десь
+            </div>
+          </div>
+
+          <div class="col text-center">
+            <div class="text-h3 text-weight-light custom-font text-white">
+               Щось
+            </div>
+          </div>
+
+          <div class="col text-center">
+            <div class="text-h1 text-white custom-font">
+              2&deg;C
+            </div>
+          </div>
+
+          <div class="weather-icon">
+            <!-- <img :src="getWeatherIconReplacer(weatherData.weather[0].icon)" /> -->
+            <img src="https://cdn-icons-png.flaticon.com/128/9755/9755232.png" />
+          </div>
+          
+        </div>
+      </div>
+    </template>
+
+    
 
   </q-page>
 </template>
