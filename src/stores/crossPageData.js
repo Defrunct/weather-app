1d10f823d6c16b278482dce7ec0576d81eaccb23
Nick Defrunct <80599378+Defrunct@users.noreply.github.com>
2025-01-13 15:00:51 +0200

    Update crossPageData.js

--- src/stores/crossPageData.js
+++ src/stores/crossPageData.js
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 export const crossPageData = defineStore('cpd', {
   state: () => ({
     currentCityName: null,
-    apiKey: 'dd76f1d7c2c54c9250d0de544316dcca'
+    apiKey: ''
   })
 })
